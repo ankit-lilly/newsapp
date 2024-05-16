fe9c46c2e73bf8446db162a83f9d2c9075c3157d
Ankit Bahuguna <bahuguna_ankit@lilly.com>
2024-05-17 00:46:54 +0530

    Missing Files

---
+++ pkg/shared/constants.go
@@ -0,0 +1,13 @@
+package shared
+
+var Categories = map[string]string{
+	"National":      "news/national",
+	"International": "news/international",
+  "Business": "business",
+	"Opinion":       "opinion",
+	"Sports":        "sport",
+  "Entertainment": "entertainment",
+  "Science": "sci-tech/science",
+  "Life & Style": "life-and-style",
+}
+
